6e3da6b5c69d6bf53b4b154c79e5ee17183a1eee
sheimbrodt <heimbrodt@uni-potsdam.de>
2024-08-27 11:52:32 +0200

    added stringifyer

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use sktlib::core::deployment::Deployment;
 use sktlib::core::service::Service;
+use sktlib::stringifyer::build_string;
 
 fn main() {
-    let d = Deployment::new("test".to_string(), "test:1.0.0".to_string());
-    let s = Service::new("testsvc".to_string(), "testdeployment".to_string());
-    let yaml = serde_yml::to_string(&d).unwrap();
-    let yaml2 = serde_yml::to_string(&s).unwrap();
-
-    println!("{}", yaml);
-    println!("{}", yaml2);
+    let s = build_string(
+        Service::new(
+            "test".to_string(),
+            "test-backend".to_string()
+        )
+    );
+    let d = build_string(
+        Deployment::new(
+            "test".to_string(),
+            "test-image".to_string()
+        )
+    );
+    println!("{}", s);
+    println!("{}", d);
 }
